f86a0cf9f7539dff377bee6d0abebd39f6c979cf
Mohadeseh76 <141071219+Mohadeseh76@users.noreply.github.com>
2023-09-19 19:55:23 +0330

    Update contact_list.py

--- contact_list.py
+++ contact_list.py
@@ -12,28 +12,34 @@ class PhoneBookApp(QWidget):
         self.loadContacts()
 
     def initUI(self):
+        # Set up the main window
         self.setWindowTitle('Phone Book')
         self.resize(1400, 400)
 
+        # Create a scrollable area to display the table
         self.scrollArea = QScrollArea(self)
         self.scrollArea.setWidgetResizable(True)
 
+        # Create a table widget to display contacts
         self.tableWidget = QTableWidget(self.scrollArea)
         self.scrollArea.setWidget(self.tableWidget)
 
+        # Set the column headers for the table
         self.tableWidget.setColumnCount(4)
         self.tableWidget.setHorizontalHeaderLabels(['Name', 'Number', 'Job', 'Email'])
 
-        # تغییر رنگ زمینه هدرهای جدول به زرد با استفاده از CSS
+        # Set the style for the table headers
         header_style = "QHeaderView::section { background-color: #8DB6CD; font-size: 18px; font-weight: bold; }"
         self.tableWidget.horizontalHeader().setStyleSheet(header_style)
 
+        # Configure table properties
         self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.tableWidget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
         self.tableWidget.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
         self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
-        self.tableWidget.setHorizontalScrollMode(QTableWidget.ScrollPerPixel)  # افزودن اسکرول بار افقی
+        self.tableWidget.setHorizontalScrollMode(QTableWidget.ScrollPerPixel)
 
+        # Create buttons for various actions
         self.searchButton = QPushButton('Search', self)
         self.searchButton.setFixedSize(115, 30)
         self.searchButton.clicked.connect(self.searchContacts)
@@ -56,7 +62,7 @@ class PhoneBookApp(QWidget):
 
         self.searchLineEdit = QLineEdit(self)
         self.searchLineEdit.setPlaceholderText('Search')
-        self.searchLineEdit.setFixedWidth(115)  # Set a fixed width for the search bar
+        self.searchLineEdit.setFixedWidth(115)
 
         buttonLayout = QVBoxLayout()
         buttonLayout.addWidget(self.searchLineEdit)
@@ -72,9 +78,8 @@ class PhoneBookApp(QWidget):
         mainLayout.addLayout(buttonLayout)
 
         self.setLayout(mainLayout)
-        
-        # ایجاد یک فونت جدید با سایز بزرگ و ضخامت بیشتر
-        button_font = QFont("Arial", 12)  # اینجا می‌توانید سایز و نوع فونت مورد نظر خود را تنظیم کنید
+
+        button_font = QFont("Arial", 12)
         button_font.setBold(True)
 
         self.searchButton.setFont(button_font)
@@ -90,12 +95,15 @@ class PhoneBookApp(QWidget):
         self.deleteButton.setStyleSheet(button_style)
         self.clearAllButton.setStyleSheet(button_style)
 
-        input_font = QFont("Arial", 16)  # اینجا می‌توانید سایز و نوع فونت مورد نظر خود را تنظیم کنید
+        input_font = QFont("Arial", 16)
         self.searchLineEdit.setFont(input_font)
 
     def createDatabase(self):
+        # Connect to or create a SQLite database
         self.conn = sqlite3.connect('phonebook.db')
         self.cur = self.conn.cursor()
+
+        # Create a "contacts" table if it doesn't exist
         self.cur.execute('''CREATE TABLE IF NOT EXISTS contacts
                             (id INTEGER PRIMARY KEY AUTOINCREMENT,
                              name TEXT,
@@ -105,36 +113,34 @@ class PhoneBookApp(QWidget):
         self.conn.commit()
 
     def loadContacts(self):
+        # Retrieve contacts from the database and populate the table
         self.cur.execute("SELECT name, number, job, email FROM contacts")
         contacts = self.cur.fetchall()
 
         self.tableWidget.setRowCount(len(contacts))
 
-        # تغییر فونت مخاطبان به حروف بزرگتر
-        contact_font = QFont("Arial", 12)  # اینجا می‌توانید سایز و نوع فونت مورد نظر خود را تنظیم کنید
+        contact_font = QFont("Arial", 12)
 
         for row, contact in enumerate(contacts):
             for col, value in enumerate(contact):
                 item = QTableWidgetItem(str(value))
-                
-                # تنظیم فونت برای هر سلول
                 item.setFont(contact_font)
-                
                 self.tableWidget.setItem(row, col, item)
                 self.tableWidget.setRowHeight(row, 80)
 
-        # تنظیم رنگ سلول‌های عمودی به ترتیب
         for row in range(self.tableWidget.rowCount()):
             if row % 2 == 0:
-                cell_color = QColor("#B8B8B8")  # رنگ طوسی
+                cell_color = QColor("#B8B8B8")  # Gray color
             else:
-                cell_color = QColor("white")  # رنگ سفید
+                cell_color = QColor("white")
 
             for col in range(self.tableWidget.columnCount()):
                 item = self.tableWidget.item(row, col)
                 if item:
                     item.setBackground(cell_color)
+
     def showAddDialog(self):
+        # Create a dialog for adding a new contact
         dialog = QDialog(self)
         dialog.setWindowTitle('Add Contact')
         dialog.setGeometry(800, 400, 400, 200)
@@ -167,6 +173,7 @@ class PhoneBookApp(QWidget):
         dialog.exec_()
 
     def saveContact(self, nameLineEdit, numberLineEdit, jobLineEdit, emailLineEdit):
+        # Save a new contact to the database
         name = nameLineEdit.text()
         number = numberLineEdit.text()
         job = jobLineEdit.text()
@@ -177,32 +184,28 @@ class PhoneBookApp(QWidget):
         self.conn.commit()
 
         self.loadContacts()
-        
+
         nameLineEdit.clear()
         numberLineEdit.clear()
         jobLineEdit.clear()
         emailLineEdit.clear()
 
-    def __del__(self):
-        self.conn.close()
-
     def deleteContact(self):
+        # Delete a selected contact from the table and database
         selected_items = self.tableWidget.selectedItems()
         if not selected_items:
             return
 
-        # Get the name of the selected contact (the first column)
         selected_name = selected_items[0].text()
 
-        # Delete the contact from the database
         self.cur.execute("DELETE FROM contacts WHERE name=?", (selected_name,))
         self.conn.commit()
 
-        # Remove the selected row from the table
         selected_row = self.tableWidget.currentRow()
         self.tableWidget.removeRow(selected_row)
 
     def clearAllContacts(self):
+        # Clear all contacts from the table and database
         msg_box = QMessageBox.question(self, 'Clear All Contacts', 'Are you sure you want to clear all contacts?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
         if msg_box == QMessageBox.Yes:
             self.cur.execute("DELETE FROM contacts")
@@ -210,14 +213,13 @@ class PhoneBookApp(QWidget):
             self.tableWidget.setRowCount(0)
 
     def editContact(self):
+        # Edit a selected contact's details
         selected_items = self.tableWidget.selectedItems()
         if not selected_items:
             return
 
-        # Get the name of the selected contact (the first column)
         selected_name = selected_items[0].text()
 
-        # Retrieve the contact details from the database
         self.cur.execute("SELECT * FROM contacts WHERE name=?", (selected_name,))
         contact = self.cur.fetchone()
         if not contact:
@@ -259,6 +261,7 @@ class PhoneBookApp(QWidget):
         dialog.exec_()
 
     def saveEditedContact(self, nameLineEdit, numberLineEdit, jobLineEdit, emailLineEdit):
+        # Save edited contact details to the database
         name = nameLineEdit.text()
         number = numberLineEdit.text()
         job = jobLineEdit.text()
@@ -268,10 +271,8 @@ class PhoneBookApp(QWidget):
         if not selected_items:
             return
 
-        # Get the name of the selected contact (the first column)
         selected_name = selected_items[0].text()
 
-        # Update the contact details in the database
         self.cur.execute("UPDATE contacts SET name=?, number=?, job=?, email=? WHERE name=?",
                          (name, number, job, email, selected_name))
         self.conn.commit()
@@ -284,22 +285,229 @@ class PhoneBookApp(QWidget):
         emailLineEdit.clear()
 
     def searchContacts(self):
+        # Search for contacts based on the provided search text
         search_text = self.searchLineEdit.text()
-        
+
         if not search_text:
             self.loadContacts()
         else:
             self.cur.execute("SELECT name, number, job, email FROM contacts WHERE name LIKE ?", ('%' + search_text + '%',))
             search_results = self.cur.fetchall()
-            
+
             self.tableWidget.setRowCount(0)
-            
+
             for row, contact in enumerate(search_results):
                 self.tableWidget.insertRow(row)
                 for col, value in enumerate(contact):
                     item = QTableWidgetItem(str(value))
                     self.tableWidget.setItem(row, col, item)
-        
+
+        self.searchLineEdit.clear()
+
+    def createDatabase(self):
+        # Connect to or create a SQLite database
+        self.conn = sqlite3.connect('phonebook.db')
+        self.cur = self.conn.cursor()
+
+        # Create a "contacts" table if it doesn't exist
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS contacts
+                            (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                             name TEXT,
+                             number TEXT,
+                             job TEXT,
+                             email TEXT)''')
+        self.conn.commit()
+
+    def loadContacts(self):
+        # Retrieve contacts from the database and populate the table
+        self.cur.execute("SELECT name, number, job, email FROM contacts")
+        contacts = self.cur.fetchall()
+
+        self.tableWidget.setRowCount(len(contacts))
+
+        contact_font = QFont("Arial", 12)
+
+        for row, contact in enumerate(contacts):
+            for col, value in enumerate(contact):
+                item = QTableWidgetItem(str(value))
+                item.setFont(contact_font)
+                self.tableWidget.setItem(row, col, item)
+                self.tableWidget.setRowHeight(row, 80)
+
+        for row in range(self.tableWidget.rowCount()):
+            if row % 2 == 0:
+                cell_color = QColor("#B8B8B8")  # Gray color
+            else:
+                cell_color = QColor("white")
+
+            for col in range(self.tableWidget.columnCount()):
+                item = self.tableWidget.item(row, col)
+                if item:
+                    item.setBackground(cell_color)
+
+    def showAddDialog(self):
+        # Create a dialog for adding a new contact
+        dialog = QDialog(self)
+        dialog.setWindowTitle('Add Contact')
+        dialog.setGeometry(800, 400, 400, 200)
+
+        formLayout = QFormLayout()
+
+        nameLineEdit = QLineEdit()
+        numberLineEdit = QLineEdit()
+        jobLineEdit = QLineEdit()
+        emailLineEdit = QLineEdit()
+
+        input_font = QFont("Arial", 16)
+        nameLineEdit.setFont(input_font)
+        numberLineEdit.setFont(input_font)
+        jobLineEdit.setFont(input_font)
+        emailLineEdit.setFont(input_font)
+
+        formLayout.addRow('Name:', nameLineEdit)
+        formLayout.addRow('Number:', numberLineEdit)
+        formLayout.addRow('Job:', jobLineEdit)
+        formLayout.addRow('Email:', emailLineEdit)
+
+        saveButton = QPushButton('Save', dialog)
+        saveButton.clicked.connect(lambda: self.saveContact(nameLineEdit, numberLineEdit, jobLineEdit, emailLineEdit))
+
+        formLayout.addRow(saveButton)
+
+        dialog.setLayout(formLayout)
+
+        dialog.exec_()
+
+    def saveContact(self, nameLineEdit, numberLineEdit, jobLineEdit, emailLineEdit):
+        # Save a new contact to the database
+        name = nameLineEdit.text()
+        number = numberLineEdit.text()
+        job = jobLineEdit.text()
+        email = emailLineEdit.text()
+
+        self.cur.execute("INSERT INTO contacts (name, number, job, email) VALUES (?, ?, ?, ?)",
+                         (name, number, job, email))
+        self.conn.commit()
+
+        self.loadContacts()
+
+        nameLineEdit.clear()
+        numberLineEdit.clear()
+        jobLineEdit.clear()
+        emailLineEdit.clear()
+
+    def deleteContact(self):
+        # Delete a selected contact from the table and database
+        selected_items = self.tableWidget.selectedItems()
+        if not selected_items:
+            return
+
+        selected_name = selected_items[0].text()
+
+        self.cur.execute("DELETE FROM contacts WHERE name=?", (selected_name,))
+        self.conn.commit()
+
+        selected_row = self.tableWidget.currentRow()
+        self.tableWidget.removeRow(selected_row)
+
+    def clearAllContacts(self):
+        # Clear all contacts from the table and database
+        msg_box = QMessageBox.question(self, 'Clear All Contacts', 'Are you sure you want to clear all contacts?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if msg_box == QMessageBox.Yes:
+            self.cur.execute("DELETE FROM contacts")
+            self.conn.commit()
+            self.tableWidget.setRowCount(0)
+
+    def editContact(self):
+        # Edit a selected contact's details
+        selected_items = self.tableWidget.selectedItems()
+        if not selected_items:
+            return
+
+        selected_name = selected_items[0].text()
+
+        self.cur.execute("SELECT * FROM contacts WHERE name=?", (selected_name,))
+        contact = self.cur.fetchone()
+        if not contact:
+            return
+
+        dialog = QDialog(self)
+        dialog.setWindowTitle('Edit Contact')
+        dialog.setGeometry(800, 400, 400, 200)
+
+        formLayout = QFormLayout()
+
+        nameLineEdit = QLineEdit()
+        nameLineEdit.setText(contact[1])
+        numberLineEdit = QLineEdit()
+        numberLineEdit.setText(contact[2])
+        jobLineEdit = QLineEdit()
+        jobLineEdit.setText(contact[3])
+        emailLineEdit = QLineEdit()
+        emailLineEdit.setText(contact[4])
+
+        input_font = QFont("Arial", 16)
+        nameLineEdit.setFont(input_font)
+        numberLineEdit.setFont(input_font)
+        jobLineEdit.setFont(input_font)
+        emailLineEdit.setFont(input_font)
+
+        formLayout.addRow('Name:', nameLineEdit)
+        formLayout.addRow('Number:', numberLineEdit)
+        formLayout.addRow('Job:', jobLineEdit)
+        formLayout.addRow('Email:', emailLineEdit)
+
+        saveButton = QPushButton('Save', dialog)
+        saveButton.clicked.connect(lambda: self.saveEditedContact(nameLineEdit, numberLineEdit, jobLineEdit, emailLineEdit))
+
+        formLayout.addRow(saveButton)
+
+        dialog.setLayout(formLayout)
+
+        dialog.exec_()
+
+    def saveEditedContact(self, nameLineEdit, numberLineEdit, jobLineEdit, emailLineEdit):
+        # Save edited contact details to the database
+        name = nameLineEdit.text()
+        number = numberLineEdit.text()
+        job = jobLineEdit.text()
+        email = emailLineEdit.text()
+
+        selected_items = self.tableWidget.selectedItems()
+        if not selected_items:
+            return
+
+        selected_name = selected_items[0].text()
+
+        self.cur.execute("UPDATE contacts SET name=?, number=?, job=?, email=? WHERE name=?",
+                         (name, number, job, email, selected_name))
+        self.conn.commit()
+
+        self.loadContacts()
+
+        nameLineEdit.clear()
+        numberLineEdit.clear()
+        jobLineEdit.clear()
+        emailLineEdit.clear()
+
+    def searchContacts(self):
+        # Search for contacts based on the provided search text
+        search_text = self.searchLineEdit.text()
+
+        if not search_text:
+            self.loadContacts()
+        else:
+            self.cur.execute("SELECT name, number, job, email FROM contacts WHERE name LIKE ?", ('%' + search_text + '%',))
+            search_results = self.cur.fetchall()
+
+            self.tableWidget.setRowCount(0)
+
+            for row, contact in enumerate(search_results):
+                self.tableWidget.insertRow(row)
+                for col, value in enumerate(contact):
+                    item = QTableWidgetItem(str(value))
+                    self.tableWidget.setItem(row, col, item)
+
         self.searchLineEdit.clear()
 
 if __name__ == '__main__':
